333fdf7aa3202d4be0c4845959975eef14a9c07b
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-04-07 13:59:39 -0600

    Coreccion en el funcionamiento de botones

--- src/Formulario/index.jsx
+++ src/Formulario/index.jsx
@@ -1,68 +1,102 @@
-import React from 'react'
+import React from 'react';
+import { URL_BACKEND } from '../common/servers';
+import Swal from 'sweetalert2';
 
-const Formulario = ({setlistaTareas}) => {
-
-    const [tarea,setTarea] = React.useState("");
+const Formulario = ({ obtenerDatos, filtrarTareas, setEstadoListas }) => {
+    const [tarea, setTarea] = React.useState("");
     const [descripcion, setDescripcion] = React.useState("");
+
+    const cargarDatos = async (tarea) => {
+        const response = await fetch(URL_BACKEND, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(tarea)
+        });
+        
+        if (response.status === 201) {
+            Swal.fire({
+                title: '¡Tarea creada!',
+                text: 'Tu tarea fue agregada con éxito',
+                icon: 'success',
+                showConfirmButton: false,
+                timer: 1800,
+                toast: true,
+                position: 'top-end',
+                timerProgressBar: true
+            });
+            obtenerDatos();
+        }
+    }
 
     const eventoFormulario = (evt) => {
         evt.preventDefault();
-        const nuevaTarea ={
-            titulo: tarea,
-            descripcion,
-            status: false
+        const nuevaTarea = {
+            name: tarea,
+            description: descripcion,
         }
-        setlistaTareas((historial) => [nuevaTarea,  ...historial])
+        cargarDatos(nuevaTarea);
         setTarea("");
         setDescripcion("");
     }
 
-
-
-  return (
-    <>
-        <form
-        onSubmit={eventoFormulario}
-        className='flex flex-column col-9 shadow p-3 redounder mt-4'>
-            <h2 className='text-center'>TO-DO LIST</h2>
-            <div className='input-group mb-3 col-12'>
-                <label className='input-group-text'>
-                    <i className='bi bi-list-task me-1'></i>
+    return (
+        <>
+            <form onSubmit={eventoFormulario} className='flex flex-column col-9 shadow p-3 rounded mt-4'>
+                <h2 className='text-center'>TO-DO LIST</h2>
+                <div className='input-group mb-3 col-12'>
+                    <label className='input-group-text'>
+                        <i className='bi bi-list-task me-1'></i>
                     </label>
-                <input 
-                    type='text'
-                    placeholder='Tarea'
-                    onChange={(evt)=>setTarea(evt.target.value)}
-                    value={tarea}
-                    className='form-control'
-                    required/>
-            </div>
-            <div className='input-group mb-3 col-12'>
-                <label className='input-group-text'>
-                    <i className="bi bi-chat me-1"></i>
-                </label>
-                <input 
-                    type="text"
-                    onChange={evt=>{
-                        setDescripcion(evt.target.value)
-                        //console.log(descripcion);
-                    }}
-                    value={descripcion}
-                    placeholder='Descripcion'
-                    className='form-control'
-                    required/>
-            </div>
-            
-            <button className='btn btn-primary col-12'>Agregar</button>
-        </form>
-        <div className='btn-group mt-3 col-9'>
-            <button className='btn btn-outline-info col-4'>Todos </button>
-            <button className='btn btn-info col-4'>Pendiente</button>
-            <button className='btn btn-info  col-4'>completado</button>
-        </div>
-    </>
+                    <input
+                        type='text'
+                        placeholder='Tarea'
+                        onChange={(evt) => setTarea(evt.target.value)}
+                        value={tarea}
+                        className='form-control'
+                        required />
+                </div>
+                <div className='input-group mb-3 col-12'>
+                    <label className='input-group-text'>
+                        <i className="bi bi-chat me-1"></i>
+                    </label>
+                    <input
+                        type="text"
+                        onChange={evt => setDescripcion(evt.target.value)}
+                        value={descripcion}
+                        placeholder='Descripcion'
+                        className='form-control'
+                        required />
+                </div>
 
-  )
+                <button className='btn btn-primary col-12'>Agregar</button>
+            </form>
+            <div className='btn-group mt-3 col-9'>
+                <button
+                    className='btn btn-outline-primary col-4'
+                    onClick={() => {
+                        setEstadoListas(0);
+                        obtenerDatos();
+                    }}
+                >
+                    Todos
+                </button>
+                <button
+                    className='btn btn-primary col-4'
+                    onClick={() => setEstadoListas(1)}
+                >
+                    Pendiente
+                </button>
+                <button
+                    className='btn btn-primary col-4'
+                    onClick={() => setEstadoListas(2)}
+                >
+                    Completado
+                </button>
+            </div>
+        </>
+    )
 }
 
-export default Formulario
+export default Formulario;  // Asegúrate de exportarlo como default
